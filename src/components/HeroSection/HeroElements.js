bf96d4dd27499d2d6c2fc18018f130222e34e0a1
Siddhant <84025779+methlox@users.noreply.github.com>
2023-03-16 02:04:17 +0530

    hero section work some some dont

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -1,29 +1,52 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const HeroContainer = styled.div`
-  background: #0c0c0c;
+export const HeroContainer = styled.section`
   display: flex;
-  justify-content: center;
+  flex-direction: row;
   align-items: center;
-  padding 0 30px;
-  height: 800px;
-  position: relative;
-  z-index: 1;
-  // width: 100%;
-  // height: 100%;
+  justify-content: space-between;
+  height: 500px;
+  background-color: #000;
+  color: #fff;
 `;
 
-export const HeroP = styled.div`
-  margin-top: 24px;
-  color: #fff;
-  font-size: 48px;
-  max-width: 600px;
+export const HeroText = styled.div`
+  max-width: 50%;
+  padding: 0 2em;
+`;
 
-  @media screen and (max-width: 768px) {
-    font-size: 24px;
-  }
+export const HeroTitle = styled.h1`
+  font-size: 3em;
+  margin-bottom: 1em;
+`;
 
-  @media screen and (max-width: 480px) {
-    font-size: 18px;
+export const HeroDescription = styled.p`
+  font-size: 1.5em;
+  margin-bottom: 2em;
+`;
+
+export const HeroButton = styled.button`
+  font-size: 1.2em;
+  background-color: #fff;
+  color: #000;
+  border: none;
+  padding: 1em 2em;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #fff;
+    color: #f00;
   }
+`;
+
+export const HeroImage = styled.div`
+  max-width: 50%;
+  height: 100%;
+`;
+
+export const HeroImageBackground = styled.img`
+  height: 100%;
+  object-fit: cover;
 `;
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,15 +1,27 @@
-import React from "react";
-import {
-  HeroContainer,
-  HeroP,
-} from "./HeroElements";
 
-const HeroSection = () => {
+
+import React from 'react';
+
+import {
+  HeroContainer, HeroText, HeroTitle, HeroDescription, HeroButton, HeroImage, HeroImageBackground
+} from './HeroElements'
+
+import bg from  '../../Media/bg1.jfif'
+
+
+const Hero = () => {
   return (
-    <HeroContainer id="home">
-      <HeroP>Department of Space, Indian Space Research Organisation</HeroP>
+    <HeroContainer>
+      <HeroText>
+        <HeroTitle>Exploring the Universe, Discovering Ourselves</HeroTitle>
+        <HeroDescription>Join us on our mission to explore space and advance scientific knowledge for the benefit of humanity.</HeroDescription>
+        <HeroButton onClick={() => {window.location.href = 'path/to/learn-more-page'}}>Learn More</HeroButton>
+      </HeroText>
+      <HeroImage>
+        <HeroImageBackground src={bg} alt="Space exploration" />
+      </HeroImage>
     </HeroContainer>
   );
 };
 
-export default HeroSection;
+export default Hero;
